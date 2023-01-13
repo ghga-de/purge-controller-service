21225d9ce38ed5c02e30a31b47e3a05070b3647e
Christoph Zwerschke <c.zwerschke@dkfz-heidelberg.de>
2023-01-13 09:25:24 +0100

    Combine and improve the template update scripts (#118)
    * combines the two scripts check_mandatory_and_static_files and update_static_files
       into one script update_template_files
    * support a check flag to distinguish between checking and actually updating
    * the new file .deprecated_files specifies all template files that have been deprecated
       and shall be removed
    * support *_ignore files that can specify a list of exceptions from the default lists
    * diffs are shown when "check" is set, so that you can see exactly what changed
    * fix deprecated set-output command in GitHub actions
    
    Co-authored-by: Kersten Breuer <kersten-breuer@outlook.com>

--- scripts/update_openapi_docs.py
+++ scripts/update_openapi_docs.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Udates OpenAPI-based documentation"""
+"""Updates OpenAPI-based documentation"""
 
 import sys
 from pathlib import Path
